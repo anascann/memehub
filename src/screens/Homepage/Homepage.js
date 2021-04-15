d28a03548ef3c37e9547b12d26e2adc7295689dd
pranjal verma <pranjal.verma150398@gmail.com>
2021-04-15 23:02:38 +0530

    added homepage/studio screens

---
+++ src/screens/Homepage/Homepage.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Container, Col, Row } from "reactstrap";
+import Thumbnail from "../../Components/Thumbnail/Thumbnail";
+import { Router, Link } from "react-router-dom";
+
+export default function Homepage() {
+  const [memes, setMemes] = useState([]);
+  let rowCount = 0;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let response = await axios.get("https://api.imgflip.com/get_memes");
+        console.log(response.data.data.memes);
+        setMemes(response.data.data.memes);
+        console.log(memes);
+      } catch (error) {
+        console.error(error);
+        setMemes([
+          {
+            url:
+              "https://freefrontend.com/assets/img/500-error-page-html-templates/HTML-500-Internal-Error.png",
+          },
+        ]);
+      }
+    };
+
+    fetchData();
+  }, []);
+  let someGlobal = [];
+  return (
+    <>
+      <Container
+      // style={{
+      //   display: "flex",
+      //   flexWrap: "wrap",
+      //   width: "60%",
+      //   //   backgroundColor: "#242B2E",
+      //   margin: "0px auto",
+      // }}
+      >
+        <Row>
+          {memes &&
+            memes.map((meme) => {
+              return (
+                <Col md={3} key={meme.id}>
+                  {/*  */}
+                  <Link to={{ pathname: "/studio", state: { ...meme } }}>
+                    <Thumbnail imgSrc={meme.url} title={meme.name} />
+                  </Link>
+                </Col>
+              );
+            })}
+        </Row>
+      </Container>
+    </>
+  );
+}
---
+++ src/screens/Studio/Studio.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import {
+  Card,
+  Container,
+  Col,
+  Row,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Button,
+} from "reactstrap";
+import axios from "axios";
+// import { Button } from "bootstrap";
+axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+export default function Studio(props) {
+  const [requestData, setRequestData] = useState({});
+  const {
+    box_count = 0,
+    url = "https://i.imgflip.com/1ur9b0.jpg",
+    memeData = {},
+    id = "",
+  } = props.location.state || {};
+  const [preview, setPreview] = useState(url);
+  console.log(props.memeData);
+  console.log("props are ++++++++++", props);
+  console.log(requestData);
+  let textFields = [];
+  for (let i = 0; i < box_count; i++) {
+    textFields.push(i);
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let axiosData = {
+        template_id: id,
+        username: "memehub1",
+        password: "pranjalanas143",
+        ...requestData,
+      };
+      // let formData = new FormData(axiosData);
+      console.log("+++++++++++++++++++++++data to be send", axiosData);
+      let response = await axios.post("https://api.imgflip.com/caption_image", {
+        axiosData,
+      });
+
+      console.log("FOrm submitted");
+      setPreview(response.data.url);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleOnChange = (event) => {
+    setRequestData((prev) => {
+      console.log(event);
+
+      return { ...prev, [event.target.name]: event.target.value };
+    });
+  };
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col xs={6}>
+            <Card>
+              <img width="100%" src={url} alt="Card image cap" />
+            </Card>
+          </Col>
+          <Col>
+            <Form id="one" name="one">
+              {textFields.map((item) => {
+                return (
+                  <FormGroup>
+                    <Label for="exampleEmail">{`Box ${item}`}</Label>
+                    <Input
+                      type="text"
+                      name={"text" + item}
+                      id={`box${item}`}
+                      onChange={handleOnChange}
+                      placeholder="write something funny"
+                    />
+                  </FormGroup>
+                );
+              })}
+              <Button type="submit" onClick={handleSubmit}>
+                {" "}
+                Make Meme
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            Preview
+            <Card>
+              <img width="100%" src={url} alt="Card image cap" />
+            </Card>
+          </Col>
+        </Row>
+        <h3>{Object.keys(requestData)}</h3>
+        <h3>{Object.values(requestData)}</h3>
+      </Container>
+    </>
+  );
+}
